42691d328d862093357eef6218b844f79ea629ed
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-11-16 15:50:25 -0500

    Add Check For Data Being a reflect.Value Type
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- decode_hooks.go
+++ decode_hooks.go
@@ -100,7 +100,12 @@ func ComposeDecodeHookFunc(fs ...DecodeHookFunc) DecodeHookFunc {
 			if err != nil {
 				return nil, err
 			}
-			newFrom = reflect.ValueOf(data)
+			if v, ok := data.(reflect.Value); ok {
+				newFrom = v
+				data = v.Interface()
+			} else {
+				newFrom = reflect.ValueOf(data)
+			}
 		}
 
 		return data, nil
